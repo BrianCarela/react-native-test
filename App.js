cb38ac702a258df25a1b8990dc94eae51fb9b8e2
BrianCarela <briancarela@gmail.com>
2019-01-30 16:35:02 -0500

    pls work

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
+
+  /////////////THIS IS AN EXAMPLE OF STATE CHANGING DYNAMICALLY
+  // constructor() {
+  //   super()
+  //   this.state = {}
+  //   this.state.customStyles = {
+  //     color: 'red'
+  //   }
+  //   setInterval(() => {
+  //     if(this.state.customStyles.color == 'red'){
+  //       this.setState({
+  //         customStyles: {
+  //           color: 'green'
+  //         }
+  //       })
+  //     } else{
+  //       this.setState({
+  //         customStyles: {
+  //           color: 'red'
+  //         }
+  //       })
+  //     }
+
+  //   }, 1000)
+  // }
+
+  // The following line is an example of passing an array of objects as a prop, so you can apply multiple styles to the same Text component
+  // [styles.welcome, this.state.customStyles]
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>WAZZAAAAAAAAA</Text>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+
+          <Text style={styles.welcome}>LALALALALALA</Text>
+        </View>
       </View>
     );
   }
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'slategray',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
   },
 });
